2106a69a13f7e6d53c7065416a21535d986922fc
mirirosca <roscamiri@gmail.com>
2024-07-19 00:07:59 +0200

    code corrections

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const axios = require('axios');
 require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY;
 
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Weather API route
 app.get('/weather', async (req, res) => {
   const city = req.query.city;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
 
   try {
+    console.log(`Fetching weather data for city: ${city}`);
     const response = await axios.get(apiUrl);
     res.json(response.data);
   } catch (error) {
-    res.status(500).send(error.message);
+    console.error('Error fetching weather data:', error.message);
+    res.status(500).send('Internal Server Error');
   }
 });
 
