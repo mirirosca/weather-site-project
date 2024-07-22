0015c797394758be020babdd4c21750ccd3ef12c
mirirosca <roscamiri@gmail.com>
2024-07-22 16:20:31 +0200

    installed npm packages

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 const express = require('express');
-const axios = require('axios');
-require('dotenv').config();
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const API_KEY = process.env.API_KEY;
 
-// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Route for the root URL
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-// Weather API route
 app.get('/weather', async (req, res) => {
   const city = req.query.city;
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+  const apiKey = process.env.API_KEY;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
   try {
-    console.log(`Fetching weather data for city: ${city}`);
-    const response = await axios.get(apiUrl);
-    res.json(response.data);
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data');
+    }
+    const data = await response.json();
+    res.json(data);
   } catch (error) {
-    console.error('Error fetching weather data:', error.message);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ error: error.message });
   }
 });
 
